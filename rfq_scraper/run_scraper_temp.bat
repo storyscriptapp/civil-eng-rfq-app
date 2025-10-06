351d321477bd68e49e12fe156f7c81db9e710151
kate <storyscriptapp@gmail.com>
2025-10-05 22:17:18 -0700

    Add .gitignore and remove database/temp files from git tracking

--- rfq_scraper/run_scraper_temp.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "C:\Users\erfer\civil-eng-rfq-app\civil-eng-rfq-app\rfq_scraper"
-"C:\Users\erfer\civil-eng-rfq-app\civil-eng-rfq-app\venv\Scripts\python.exe" multi_scraper.py --cities "Town of Queen Creek,Town of Florence"
-echo.
-echo Scraper finished. Press any key to close...
-pause > nul
